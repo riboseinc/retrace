62672c13f2b997b1dda75396e42d9eaee1554e8e
Jin JinRu <jin.jinru840430@gmail.com>
2017-06-18 20:46:16 +0800

    modified connect(), bind() functions

--- sock.h
+++ sock.h
@@ -3,20 +3,17 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <arpa/inet.h>
 
 typedef int (*rtr_socket_t)(int domain, int type, int protocol);
-
-typedef int (*rtr_atoi_t)(const char *str);
+typedef int (*rtr_connect_t)(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 typedef int (*rtr_accept_t)(int fd, struct sockaddr *address, socklen_t *len);
 typedef int (*rtr_bind_t)(int fd, const struct sockaddr *address, socklen_t len);
-typedef int (*rtr_connect_t)(int fd, const struct sockaddr *address, socklen_t len);
 
 RETRACE_DECL(socket);
-
-RETRACE_DECL(atoi);
+RETRACE_DECL(connect);
 RETRACE_DECL(accept);
 RETRACE_DECL(bind);
-RETRACE_DECL(connect);
 
 #endif /* __RETRACE_SOCK_H__ */
