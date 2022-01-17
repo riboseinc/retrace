f1f168ad62d3c7c75a27a1ab87eb14a80a5f02da
Jeffrey Lau <jeffrey.lau@ribose.com>
2022-01-17 17:16:03 +0800

    ci: Fix windows-msys build (by fixing libnereon & libucl build)
    The links [1][2] were used in helping with the build issue (with libucl):
    
        aclocal-1.16: error: aclocal: file '/a/_temp/msys64/usr/share/aclocal/xsize.m4' does not exist
    
    [1] https://github.com/kwhat/jnativehook/issues/89
    [2] https://github.com/avast/yaracpp/issues/12
    
    Related: https://github.com/riboseinc/retrace/issues/421

--- ci/before_install/msys.sh
+++ ci/before_install/msys.sh
@@ -3,14 +3,24 @@ set -ex
 
 msys_install() {
 	packages=(
+		autoconf
 		automake
-		mingw-w64-x86_64-cmocka
-		mingw64/mingw-w64-x86_64-ninja
-		mingw64/mingw-w64-x86_64-cmake
-		mingw64/mingw-w64-x86_64-graphviz # for doxygen's dot component
-		openssl-devel
+		cmake
 		doxygen
+		gcc
+		git
+		libtool
+		make
+		mingw-w64-x86_64-cmocka
+		mingw-w64-x86_64-libtool
+		mingw-w64-x86_64-pkg-config
+		mingw-w64-x86_64-toolchain
+		mingw64/mingw-w64-x86_64-graphviz # for doxygen's dot component
+		ninja
+		openssl-devel
+		pkg-config
 	)
+	# pacman --noconfirm -Syu  # NOTE: would close the current terminal, thereby failing the build
 	pacman --noconfirm -S --needed "${packages[@]}"
 	gem install mustache
 }
