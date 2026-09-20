ef1fa56ed7c11b91f2c7c15de8d8901e0e9a1766
Ronald Tse <ronald.tse@ribose.com>
2026-09-20 17:31:23 +0800

    v2.105.0: the docs and site catch up to v2.104.0
    CHANGELOG entries 2.41.0 through 2.104.0; platform docs for
    mips64, rv64, and Android/bionic; the website feed, badge,
    about paragraph, and docs page current through v2.104.0.

--- include/retrace/version.h
+++ include/retrace/version.h
@@ -38,10 +38,10 @@
 
 #define RETRACE_VERSION_MAJOR 2
 #define RETRACE_VERSION_MAJOR 2
-#define RETRACE_VERSION_MINOR 104
+#define RETRACE_VERSION_MINOR 105
 #define RETRACE_VERSION_PATCH 0
 
-#define RETRACE_VERSION_STRING "2.104.0"
+#define RETRACE_VERSION_STRING "2.105.0"
 
 #define RETRACE_VERSION_ATLEAST(maj, min, pat)                  \
 	(RETRACE_VERSION_MAJOR > (maj) ||                       \
